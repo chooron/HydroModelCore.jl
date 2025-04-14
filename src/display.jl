18a58345f994a1eb454f338f8a8c72245fd33f80
jingx <50790703+chooron@users.noreply.github.com>
2025-04-14 09:10:24 +0800

    Update display.jl

--- src/display.jl
+++ src/display.jl
@@ -137,7 +137,6 @@ function Base.show(io::IO, uh::AbstractHydrograph)
         print(io, "inputs=[", join(get_input_names(uh), ","), "]")
         print(io, ", outputs=[", join(get_output_names(uh), ","), "]")
         print(io, ", params=[", join(get_param_names(uh), ","), "]")
-        print(io, ", uhfunc: ", typeof(uh.uhfunc).parameters[1])
         print(io, ")")
     else
         printstyled(io, "┌ ", color=:light_blue, bold=true)
@@ -159,15 +158,16 @@ function Base.show(io::IO, uh::AbstractHydrograph)
         printstyled(io, "Parameters:   ", color=:light_magenta)
         println(io, "[", join(get_param_names(uh), ", "), "]")
         
-        # Print UnitFunction with a different color
-        print(io, "│ ")
-        printstyled(io, "UnitFunction: ", color=:cyan)
-        println(io, typeof(uh.uhfunc).parameters[1])
+        # todo 还没想好怎么打印这个
+        # # Print UnitFunction with a different color
+        # print(io, "│ ")
+        # printstyled(io, "UnitFunction: ", color=:cyan)
+        # println(io, typeof(uh.uhfunc).parameters[1])
         
-        # Print SolveType with a different color
-        print(io, "│ ")
-        printstyled(io, "SolveType:    ", color=:cyan)
-        println(io, typeof(uh).parameters[end])
+        # # Print SolveType with a different color
+        # print(io, "│ ")
+        # printstyled(io, "SolveType:    ", color=:cyan)
+        # println(io, typeof(uh).parameters[end])
         
         printstyled(io, "└─", color=:light_blue)
         println(io)
@@ -221,9 +221,9 @@ function Base.show(io::IO, ele::AbstractBucket)
         printstyled(io, "Fluxes:", color=:yellow)
         println(io)
         for flux in ele.fluxes
-            for ex in get_exprs(flux)
+            for (output, ex) in zip(get_outputs(flux), get_exprs(flux))
                 print(io, "│   ")
-                println(io, get_outputs(flux) ~ ex)
+                println(io, output ~ ex)
             end
         end
 
@@ -234,9 +234,9 @@ function Base.show(io::IO, ele::AbstractBucket)
             printstyled(io, "State Fluxes:", color=:yellow)
             println(io)
             for dflux in ele.dfluxes
-                for ex in get_exprs(dflux)
+                for (st, ex) in zip(get_states(dflux), get_exprs(dflux))
                     print(io, "│   ")
-                    println(io, get_outputs(dflux) ~ ex)
+                    println(io, st ~ ex)
                 end
             end
         end
@@ -254,10 +254,11 @@ function Base.show(io::IO, route::AbstractHydroRoute)
         print(io, ", states=[", join(get_state_names(route), ","), "]")
         print(io, ", outputs=[", join(get_output_names(route), ","), "]")
         print(io, ", params=[", join(get_param_names(route), ","), "]")
+        print(io, ", nns=[", join(get_nn_names(route), ","), "]")
         print(io, ")")
     else
         printstyled(io, "┌ ", color=:light_blue, bold=true)
-        printstyled(io, "HydroRoute", color=:light_blue, bold=true)
+        printstyled(io, "HydroBucket", color=:light_blue, bold=true)
         printstyled(io, "{$(get_name(route))}", color=:light_black)
         println(io)
         
@@ -281,6 +282,34 @@ function Base.show(io::IO, route::AbstractHydroRoute)
         printstyled(io, "Params:  ", color=:light_magenta)
         println(io, "[", join(get_param_names(route), ", "), "]")
         
+        # Print neural networks with a different color
+        print(io, "│ ")
+        printstyled(io, "NNs:     ", color=:light_cyan)
+        println(io, "[", join(get_nn_names(route), ", "), "]")
+
+        # Print fluxes expressions
+        println(io, "│")
+        print(io, "│ ")
+        printstyled(io, "Fluxes:", color=:yellow)
+        println(io)
+        for flux in route.fluxes
+            for (output, ex) in zip(get_outputs(flux), get_exprs(flux))
+                print(io, "│   ")
+                println(io, output ~ ex)
+            end
+        end
+
+        println(io, "│")
+        print(io, "│ ")
+        printstyled(io, "State Fluxes:", color=:yellow)
+        println(io)
+        for dflux in route.dfluxes
+            for (st, ex) in zip(get_states(dflux), get_exprs(dflux))
+                print(io, "│   ")
+                println(io, st ~ ex)
+            end
+        end
+        
         printstyled(io, "└─", color=:light_blue)
         println(io)
     end
@@ -290,6 +319,7 @@ function Base.show(io::IO, model::AbstractModel)
     fluxes_in_model = filter(x -> x isa AbstractFlux, model.components)
     buckets_in_model = filter(x -> x isa AbstractBucket, model.components)
     routes_in_model = filter(x -> x isa AbstractRoute, model.components)
+    uh_in_model = filter(x -> x isa AbstractHydrograph, model.components)
     @assert(length(routes_in_model) <= 1, "Only one route is allowed in a model")
     compact = get(io, :compact, false)
     if compact
@@ -347,6 +377,11 @@ function Base.show(io::IO, model::AbstractModel)
         print(io, "│   ")
         printstyled(io, "Routes:  ", color=:light_cyan)
         println(io, length(routes_in_model), " route", length(routes_in_model) == 1 ? "" : "s")
+
+        # Print routes count
+        print(io, "│   ")
+        printstyled(io, "UnitHydrographs:  ", color=:light_cyan)
+        println(io, length(uh_in_model), " route", length(uh_in_model) == 1 ? "" : "s")
         
         printstyled(io, "└─", color=:light_blue)
         println(io)
